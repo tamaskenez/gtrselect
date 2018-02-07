ee840de0dffb9374ad413970de4debdc2aec20de
Tamas Kenez <tamas.kenez@gmail.com>
2018-02-07 22:24:40 +0100

    Bigger images.

--- mining_scripts/img_postprocess.sh
+++ mining_scripts/img_postprocess.sh
@@ -3,5 +3,5 @@ DIR=$RESDIR/imgs_table_horiz
 rm -rf $DIR
 mkdir $DIR
 for a in $RESDIR/imgs/*; do
-    gm convert -resize x200 -rotate 90 $a $DIR/$(basename $a)
+    gm convert -resize x400 -rotate 90 $a $DIR/$(basename $a)
 done
